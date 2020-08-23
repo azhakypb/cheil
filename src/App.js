5ad8458dd6ea495815ea3c21c63c18e912a38ed5
Adil_MSI <zhakypadil@gmail.com>
2020-08-23 16:20:47 +0600

    Validation Completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import './index.css'
 import UpperHeader from './components/UpperHeader';
--- src/components/ValidationForm.js
+++ src/components/ValidationForm.js
@@ -1,83 +1,90 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
-import { Formik } from 'formik'
-import * as Yup from 'yup';
+import React from 'react'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 import "yup-phone";
+import { Button } from 'react-bootstrap';
 
-const ValidationForm = () => (
-    <Formik
-        initialValues = {{ fullname: "", email: "", phone: "" }}
-        onSubmit = {(values, {setSubmitting }) => {
-            setTimeout(() => {
-                console.log("Logging in", values);
-            }, 500);
-        }}
+const initialValues = {
+  name: '',
+  email: '',
+  phone: ''
+}
 
-        // validationSchema = {Yup.object().shape({
-        //     fullname: Yup.string()
-        //         .name()
-        //         .required("Укажите Ваше Имя"),
+const onSubmit = values => {
+  console.log('Form data', values)
+}
 
-        //     email: Yup.string()
-        //         .email()
-        //         .required("Required"),
-            
-        //     phone: Yup.string()
-        //         .phone("KZ")
-        //         .required("Required")
-            
-        // })}
-    >
-        {props => {
-            const {
-                values,
-                touched,
-                errors,
-                isSubmitting,
-                handleChange,
-                handleBlur,
-                handleSubmit
-            } = props;
+const validationSchema = Yup.object({
+  name: Yup.string().required('Поле Обязательно Для Заполнения'),
+  email: Yup.string()
+    .email('Почта введена не верно, формат: example@example.example')
+    .required('Required'),
+  phone: Yup.string().required('Поле Обязтельно Для Заполнения').phone("KZ")
+})
 
-            return (
-                <form autoComplete="off" onSubmit={handleSubmit} class="validForm">
-                <div class="form-group">
-                    <label htmlFor="fullName">ФИО</label>
-                    <input 
-                        type="name" 
-                        value={values.fullname}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        class="form-control"
-                        />
-                </div>
-                <div class="form-group">
-                    <label htmlFor="email">Ваш email</label>
-                    <input 
-                        type="text" 
-                        name="email" 
-                        value={values.email}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        class="form-control"
-                        />
-                </div>
-                <div class="form-group">
-                    <label htmlFor="phone">Телефон</label>
-                    <input 
-                        type="tel" 
-                        value={values.phone}
-                        onChange={handleChange}
-                        onBlur={handleBlur} 
-                        class="form-control"
-                        />
-                </div>
-                <div class="text-center"><Button type="submit" disabled={isSubmitting} variant="outline-dark bt1 bt2">ОТПРАВИТЬ ДАННЫЕ</Button></div>
-            </form>
-            )
-        }}
+function ValidationForm () {
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    // validate,
+    validationSchema
+  })
 
-    </Formik>
-);
+  //console.log('formik.touched', formik.touched)
 
-export default ValidationForm;
+  return (
+    <form onSubmit={formik.handleSubmit} class="validForm">
+      <div class='form-group'>
+        <label htmlFor='name'>ФИО</label>
+        <input
+          type='text'
+          id='name'
+          name='name'
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.name}
+          class="form-control"
+        />
+        {formik.touched.name && formik.errors.name ? (
+          <div className='error'>{formik.errors.name}</div>
+        ) : null}
+      </div>
+
+      <div className='form-group'>
+        <label htmlFor='email'>Электронная почта</label>
+        <input
+          type='email'
+          id='email'
+          name='email'
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
+          class="form-control"
+        />
+        {formik.touched.email && formik.errors.email ? (
+          <div className='error'>{formik.errors.email}</div>
+        ) : null}
+      </div>
+
+      <div className='form-group'>
+        <label htmlFor='phone'>Номер телефона</label>
+        <input
+          type='text'
+          id='phone'
+          name='phone'
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.phone}
+          class="form-control"
+        />
+        {formik.touched.phone && formik.errors.phone ? (
+          <div className='error'>{formik.errors.phone}</div>
+        ) : null}
+      </div>
+
+      <div class="text-center"><Button type="submit" variant="outline-dark bt1 bt2">ОТПРАВИТЬ ДАННЫЕ</Button></div>
+    </form>
+  )
+}
+
+export default ValidationForm
